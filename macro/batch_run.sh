d4cfb57943b157bcf258193586d09883c8745363
mam-mih-val <mam.mih.val@gmail.com>
2021-04-01 18:59:41 +0300

    testing setup

--- macro/batch_run.sh
+++ macro/batch_run.sh
@@ -15,7 +15,8 @@ cd $job_num
 echo "loading " $hadesroot
 source $hadesroot
 
-for start in 0 10000 20000 30000 40000
+#for start in 0 10000 20000 30000 40000
+for start in 0
 do
   mkdir $start
   cd $start
@@ -29,14 +30,14 @@ do
   echo $(pwd)/input.evt >> config.dat
   echo $(pwd)/geant_out.root >> config.dat
 
-  /cvmfs/hades.gsi.de/install/5.34.34/hgeant2-5.5/hgeant -c -f config.dat no no no
+  /cvmfs/hades.gsi.de/install/5.34.34/hgeant2-5.5a/hgeant -c -f config.dat no no no
   mkdir dst
   ls geant_out*.root > geant_out.list
   while read line; do
     input_files=$input_files","$line
   done < geant_out.list
-  echo "executing /lustre/hebe/hades/user/fkornas/sub/apr12/OwnEvt/dst/analysisDST geant_out1.root dst/ 10000 1"
-  /lustre/hebe/hades/user/fkornas/sub/apr12/OwnEvt/dst/analysisDST geant_out1.root dst/ 10000
+  echo "executing /lustre/nyx/hades/user/mmamaev/dst_agag/analysisDST geant_out1.root dst/ 10000 1"
+  /lustre/nyx/hades/user/mmamaev/dst_agag/analysisDST geant_out1.root dst/ 10000
   cd ..
 done
 
